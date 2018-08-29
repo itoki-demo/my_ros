3c60591ce34aaa28777e269adecacee47b61ee53
ayato-mizutani <mizutani83ha@gmail.com>
2018-08-29 17:19:29 +0900

    switchbot

--- src/switchbot/src/switchbot_action.py
+++ src/switchbot/src/switchbot_action.py
@@ -2,11 +2,18 @@
 import rospy
 from std_msgs.msg import String
 
-if __name__ == '__main__':
+def switchbot_action():
     rospy.init_node('switchbot_controller')
     pub = rospy.Publisher('switchbot_action',String, queue_size = 10)
     r = rospy.Rate(1)
     while True:
-        r.sleep()
         pub.publish('action')
-    
+        r.sleep()
+
+if __name__ == '__main__':
+
+    try:
+        switchbot_action()
+    except rospy.ROSInterruptException:
+        pass
+ 
