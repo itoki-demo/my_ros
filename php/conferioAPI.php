fa642d6d6a88855477eb18aa00f5b5c0366fdfa4
ayato-mizutani <mizutani83ha@gmail.com>
2018-09-13 11:52:46 +0900

    conf php

--- php/conferioAPI.php
+++ php/conferioAPI.php
@@ -1,14 +1,49 @@
 <?php
-#init_set("display_errors", 1);
-#error_reporting(E_ALL);
+
 $b_Num = "";
-$confRec = "http://192.168.10.136/rapyutaJSON.php?b_Num=";
+$b_Name = "";
+
 if(isset($_GET['b_Num'])){
-	$b_Num = $_GET['b_Num'];
-	$confRec .= $b_Num;
-	#echo $confRec;
+ $b_Num = $_GET['b_Num'];
 }
-$contents = array();
-$contents = file_get_contents($confRec);
-echo $contents;
+ 
+$fpath = 'conferioData.csv';
+$lines = file($fpath);
+$count = count(file($fpath));
+
+$data_json = array();
+$data_array = array();
+
+foreach($lines as $line){
+ $list = preg_split("/,/", $line);
+ $data = array('b_Num'=> $list[0], 'b_ID'=> $list[1], 'b_Name'=> $list[2], 'b_RoomName'=> $list[3],'b_Date'=> $list[4], 'b_StartTime'=> $list[5], 'b_FinishTime'=> $list[6], 'b_Time'=> trim($list[7]));
+ $data_json[] = json_encode($data);
+};
+
+$i = 0;
+$flag = false;
+for(;$i < $count; $i++){
+ 
+ $data_array = json_decode($data_json[$i], true);
+ //echo $data_json[$i];
+ //echo $data_array[b_Num];
+ //print($data_array['b_Num']);
+ //print("==");
+ //print($b_Num);
+ if($data_array['b_Num'] == $b_Num){
+  $flag = true;
+  break;
+ }
+};
+
+if($flag == true){
+ //echo $data_array['b_Name'];
+ //header("Content-Type: application/json; charset=utf-8");
+ echo $data_json[$i];
+} else {
+ echo "No Such Number!";
+}
+
+
+
 ?>
