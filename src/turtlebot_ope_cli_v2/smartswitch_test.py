ab9d2a070bd643657df958e528d1124a2816b2d4
ayato mizutani <mizutani83ha@gmail.com>
2018-10-04 09:19:12 +0900

    update sendmail

--- src/turtlebot_ope_cli_v2/smartswitch_test.py
+++ src/turtlebot_ope_cli_v2/smartswitch_test.py
@@ -3,23 +3,21 @@
 
 import socket
 
-host = "192.168.20.20" #お使いのサーバーのホスト名を入れます
-port = 1111 #適当なPORTを指定してあげます
+host = "192.168.250.1" #お使いのサーバーのホスト名を入れます
+port = 23 #適当なPORTを指定してあげます
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #オブジェクトの作成をします
 
 client.connect((host, port))
-
-client.send('nwk\n') #適当なデータを送信します（届く側にわかるように）
 print(client.recv(4096))
-switch_list = ['#DEVICE,0x01e36d23,141,7,1\r\n','#DEVICE,0x01e36d23,141,7,2\r\n','#DEVICE,0x01e36d23,141,7,3\r\n','#DEVICE,0x01e36d23,141,7,4\r\n']
+client.send('nwk\r') #適当なデータを送信します（届く側にわかるように）
+
+switch_list = ['#DEVICE,0x01e36d23,141,7,1\r','#DEVICE,0x01e36d23,141,7,2\r','#DEVICE,0x01e36d23,141,7,3\r','#DEVICE,0x01e36d23,141,7,4\r']
 while True:
     print("botton number :")
     str_in = raw_input()
     num = int(str_in)
     client.send(switch_list[num-1])
-    print(client.recv(4096))
-    print(switch_list[num-1])
-    client.send('nwk\n')
-    print(client.recv(4096))
+
+
     
